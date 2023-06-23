5e3c31c551320a73013d8218fe271f2e410661af
vishhnu-dev <avitassibanac@gmail.com>
2023-06-23 16:34:48 -0300

    initial commit

---
+++ Game/Unreal/Source/GreenPiece/Public/GreenPieceGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GreenPieceGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGreenPieceGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGreenPieceGameMode();
+};
+
+
+
